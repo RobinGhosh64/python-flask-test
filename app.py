34b58d2b6ac2974aa95443e21cff644a296e0e16
RobinGhosh64 <rrrghosh@yahoo.com>
2024-02-18 21:24:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import os
+import openai
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
-
+openai.api_key = "sk-XXX"
 
 @app.route('/')
 def index():
@@ -27,6 +28,21 @@ def hello():
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
+@app.route('/openai/<question>', methods=['GET'])
+def callchatgpt(id: int):
+  response = openai.ChatCompletion.create(
+        model="gpt-4",
+        stream=True,
+        messages=
+        [{
+          "role": "user",
+          "content": question
+        }]
+    )
+  for chunk in response:
+   if 'content' in chunk.choices[0].delta:
+     return (chunk.choices[0].delta.content,end="")
 
+  
 if __name__ == '__main__':
    app.run()
